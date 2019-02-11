5cea2e934a5707379784faab190f603c2b7d96be
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2019-02-11 14:51:35 +0000

    Fix issue about scale

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ data "template_file" "rancher_compose_master" {
   template = "${file("${path.module}/rancher/elasticsearch-master/rancher-compose.yml")}"
 
   vars {
-    scale = "${var.scale != "" ? "scale: var.scale" : ""}"
+    scale  = "${var.scale != "" ? "scale: ${var.scale}" : ""}"
   }
 }
 resource "rancher_stack" "this_master" {
@@ -127,7 +127,7 @@ data "template_file" "rancher_compose_client" {
   template = "${file("${path.module}/rancher/elasticsearch-client/rancher-compose.yml")}"
 
   vars {
-    scale = "${var.scale != "" ? "scale: var.scale" : ""}"
+    scale  = "${var.scale != "" ? "scale: ${var.scale}" : ""}"
   }
 }
 resource "rancher_stack" "this_client" {
@@ -187,7 +187,7 @@ data "template_file" "rancher_compose_data" {
   template = "${file("${path.module}/rancher/elasticsearch-data/rancher-compose.yml")}"
 
   vars {
-    scale = "${var.scale != "" ? "scale: var.scale" : ""}"
+    scale  = "${var.scale != "" ? "scale: ${var.scale}" : ""}"
   }
 }
 resource "rancher_stack" "this_data" {
@@ -249,7 +249,7 @@ data "template_file" "rancher_compose_all" {
   template = "${file("${path.module}/rancher/elasticsearch-all/rancher-compose.yml")}"
 
   vars {
-    scale = "${var.scale != "" ? "scale: var.scale" : ""}"
+     scale  = "${var.scale != "" ? "scale: ${var.scale}" : ""}"
   }
 }
 resource "rancher_stack" "this_all" {
