8f9769533f31ccd873df3aede632a07b4300aabd
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2019-12-10 09:59:10 +0000

    Upgrade elasticsearch to support include/exclude audit log type

--- variables.tf
+++ variables.tf
@@ -36,7 +36,7 @@ variable "cluster_name" {
 }
 variable "image_version" {
   description = "The image version of Elasticsearch to use"
-  default = "6.8.2-3"
+  default = "6.8.2-4"
 }
 variable "elastic_password" {
   description = "The elastic password"
