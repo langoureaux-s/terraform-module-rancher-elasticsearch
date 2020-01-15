7d40b5b5e075b51e276e4aa3cc3ac695e094b9c8
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2020-01-15 13:49:49 +0000

    Use elasticsearch 7.5.1-1 as default image version

--- variables.tf
+++ variables.tf
@@ -36,7 +36,7 @@ variable "cluster_name" {
 }
 variable "image_version" {
   description = "The image version of Elasticsearch to use"
-  default = "6.8.2-6"
+  default = "7.5.1-1"
 }
 variable "elastic_password" {
   description = "The elastic password"
